d80015d7d331995ce38403fd4d5338f67e7245a5
RhobarIII <95757630+RhobarIII@users.noreply.github.com>
2022-04-05 11:18:01 +0200

    Update
    nearComplete

--- zgloszenie.php
+++ zgloszenie.php
@@ -1,1 +1,21 @@
 <?php
+$db= new mysqli("localhost", "root", "", "ratownictwo");
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+//date_default_timezone_set('Europe/Lisbon');
+$date=date('d-m-y h:i:s');
+$id_rat=$_POST['nr_of_team'];
+$id_dys=$_POST['nr_of_dispatcher'];
+$addres=$_POST['addres'];
+$query="INSERT INTO zgloszenia (ratownicy_id, dyspozytorzy_id,adres,pilne,czas_zgloszenia) VALUES('$id_rat','$id_dys','$addres',0,'$date')";
+
+if ($result=$db->query($query))
+{
+    echo "New record created successfully";
+}
+else
+{
+    echo "Error: " ;
+}
+$db->close();
